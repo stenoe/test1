e968ea3ae928a6b0333afb033304f6339bda8c8d
Steffen Noe <steffen.m.noe@gmail.com>
2019-02-27 16:08:20 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,24 @@
+echo "Copy input data, unarchive"
+
+cd data/ 
+cp ../testdata/*.tar .
+tar -xvf *.tar
+
+ls -all
+
+echo "Go to working directory"
+
+cd ../script1/
+pwd
+
+echo "Setup Display"
+
+export DISPLAY=:99
+export QT_X11_NO_MITSHM=1
+export GSHOSTNAME=boundless-test
+
+echo "Run the model"
+
+Xvfb :99 -ac -noreset & python /script1/ama_maebiastaSKA_T2M_v1.py 
+
+ls -all
